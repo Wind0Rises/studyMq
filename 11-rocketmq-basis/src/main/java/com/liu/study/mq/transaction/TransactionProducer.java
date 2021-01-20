b5aa22f1a9c5e33fc5e9da9b175f0c81c4a72b06
liuweian <1qaz2wsx>
2021-01-20 11:24:37 +0800

    rocketmq 应用级

--- 11-rocketmq-basis/src/main/java/com/liu/study/mq/transaction/TransactionProducer.java
+++
@@ -1,9 +0,0 @@
-package com.liu.study.mq.transaction;
-
-/**
- * @author lwa
- * @version 1.0.0
- * @createTime 2021/1/19 15:07
- */
-public class TransactionProducer {
-}
---
+++ 13-rocketmq-application/src/main/java/com/liu/study/mq/rocket/application/Test.java
@@ -0,0 +1,9 @@
+package com.liu.study.mq.rocket.application;
+
+/**
+ * @author lwa
+ * @version 1.0.0
+ * @createTime 2021/1/20 11:04
+ */
+public class Test {
+}
